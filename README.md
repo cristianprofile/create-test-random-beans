e2ed8c357cbc2f95deac796db11c3df42f06a368
mymoid <cromero@mymoid.com>
2018-09-26 16:09:11 +0200

    Fixed using random static method

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # create-test-random-beans
-Demo project for Random Beans inside testing methods
+Demo project for Random Beans inside testing methods.
+
+Because life is too short to generate random Java beans by hand. Do not waste you time instantiation your beans inside your test
+Delegate random-beans library to do it : (https://github.com/benas/random-beans)
+
+
+
+![class](/images/class.jpg?raw=true "class")
+
+
+     @Test
+      public void contextLoads() {
+
+
+        // create random Book
+
+        Book book = random(Book.class);
+
+        final Book book1 = bookRepository.save(book);
+
+        assertThat(book1.getDescription()).isEqualToIgnoringCase(book.getDescription());
+
+        EnhancedRandom random1 = EnhancedRandomBuilder.aNewEnhancedRandomBuilder()
+                .seed(123L)
+                .objectPoolSize(100)
+                .randomizationDepth(3)
+                .charset(forName("UTF-8"))
+                .stringLengthRange(5, 50)
+                .collectionSizeRange(0, 0)
+                .scanClasspathForConcreteTypes(true)
+                .overrideDefaultInitialization(false)
+                .build();
+
+        // create random User with custom random object --- list of books will have zero size ( collectionSizeRange(0, 0))
+        User user = random1.nextObject(User.class);
+        user.getBooks().add(book);
+        final User save = userRepository.save(user);
+
+        //if i add the the book to my user domain object  books size will be 1
+
+        final Optional<User> userFound = userRepository.findById(save.getId());
+        assertThat(userFound).isPresent();
+
+        assertThat(userFound.get().getBooks()).isNotEmpty();
+        assertThat(userFound.get().getBooks()).hasSize(1);
+        assertThat(userFound.get().getBooks().get(0).getTitle()).isEqualToIgnoringCase(book1.getTitle());
+
+    }
+
+
+
+
+
--- src/test/java/com/cromero/randombeaninstantationtesting/RandomBeanInstantationTestingApplicationTests.java
+++ src/test/java/com/cromero/randombeaninstantationtesting/RandomBeanInstantationTestingApplicationTests.java
@@ -12,15 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 import static io.github.benas.randombeans.api.EnhancedRandom.random;
-import static io.github.benas.randombeans.api.EnhancedRandom.randomCollectionOf;
 import static java.nio.charset.Charset.forName;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class RandomBeanInstantationTestingApplicationTests {
 
 
@@ -45,8 +46,7 @@ public class RandomBeanInstantationTestingApplicationTests {
                 .scanClasspathForConcreteTypes(true)
                 .overrideDefaultInitialization(false)
                 .build();
-        User user = random.random(User.class);
-
+        User user = random.nextObject(User.class);
         assertThat(user.getBooks()).hasSize(1);
     }
 
@@ -55,12 +55,13 @@ public class RandomBeanInstantationTestingApplicationTests {
     public void contextLoads() {
 
 
+        // create random Book
+
         Book book = random(Book.class);
 
         final Book book1 = bookRepository.save(book);
 
         assertThat(book1.getDescription()).isEqualToIgnoringCase(book.getDescription());
-
 
         EnhancedRandom random1 = EnhancedRandomBuilder.aNewEnhancedRandomBuilder()
                 .seed(123L)
@@ -68,19 +69,24 @@ public class RandomBeanInstantationTestingApplicationTests {
                 .randomizationDepth(3)
                 .charset(forName("UTF-8"))
                 .stringLengthRange(5, 50)
-                .collectionSizeRange(1, 10)
+                .collectionSizeRange(0, 0)
                 .scanClasspathForConcreteTypes(true)
                 .overrideDefaultInitialization(false)
                 .build();
-        User user = random1.random(User.class);
 
-        List<Book> books = ( List<Book>)randomCollectionOf(0, Book.class);
-        books.add(book);
-        user.setBooks(books);
+        // create random User with custom random object --- list of books will have zero size ( collectionSizeRange(0, 0))
+        User user = random1.nextObject(User.class);
+        user.getBooks().add(book);
         final User save = userRepository.save(user);
-//        assertThat(save.getBooks().get(0).getTitle()).isEqualToIgnoringCase(book1.getTitle());
 
+        //if i add the the book to my user domain object  books size will be 1
 
+        final Optional<User> userFound = userRepository.findById(save.getId());
+        assertThat(userFound).isPresent();
+
+        assertThat(userFound.get().getBooks()).isNotEmpty();
+        assertThat(userFound.get().getBooks()).hasSize(1);
+        assertThat(userFound.get().getBooks().get(0).getTitle()).isEqualToIgnoringCase(book1.getTitle());
 
     }
 
